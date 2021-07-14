72b13feabb735cb574519ad7f7e9ce37fecf564f
Junep <prepared@inflearn.com>
2021-07-14 16:41:51 +0900

    utils local package 추가

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { add } from '@service/utils';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
